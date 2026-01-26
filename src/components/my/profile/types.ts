32ed3d0d449ef73a0a42168f2ba8bffc1e6adf72
jiyeonyooo <ji04230423@gmail.com>
2026-01-26 21:04:42 +0900

    chore: 프로필 관련 타입 정의

---
+++ src/components/my/profile/types.ts
@@ -0,0 +1,13 @@
+export type Profile = {
+  nickname: string;
+  profileImagePath: string;
+  leaderIntro: string;
+  memberIntro: string;
+};
+
+export type Image = {
+  directory: string;
+  fileName: string;
+  contentType: string;
+  fileSize: number;
+};
